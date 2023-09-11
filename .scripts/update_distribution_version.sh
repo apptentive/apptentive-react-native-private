2458425313fdff35d288379760435b2fbded3a46
Frank Schmitt <frank.schmitt@alchemer.com>
2023-09-11 12:30:33 -0600

    Add github workflows

---
+++ .scripts/update_distribution_version.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This script should update any incidental copies of the 
+# "single source of truth" version, e.g. in package.json.
+# It will be different for each project.
+
+# Fail on first error.
+set -e
+
+# Check for correct number of arguments.
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <new.distribution.version>"
+    exit 1
+fi
+
+new_version="$1"
+podspec_file="apptentive-fake-plugin.podspec"
+
+# Check if the podspec file exists
+if [ ! -f "$podspec_file" ]; then
+    echo "Error: $podspec_file does not exist."
+    exit 1
+fi
+
+# Use sed to update the version in the podspec file
+sed -i "s/s.version\( *\)= *\"[^\"]*\"/s.version\1= \"$new_version\"/" "$podspec_file"
+
+echo "Updated version to $new_version in $podspec_file."
+
+javascript_file="www/Apptentive.js"
+
+# Check if the javascript file exists
+if [ ! -f "$javascript_file" ]; then
+    echo "Error: $javascript_file does not exist."
+    exit 1
+fi
+
+# Use sed to update the version in the javascript file
+sed -i "s/distributionVersion: \"[^\"]*\"/distributionVersion: \"$new_version\"/" "$javascript_file"
+
+echo "Updated version to $new_version in $javascript_file."
